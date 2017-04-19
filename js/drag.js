abb7e06fefbe7941ff794a7f0398c0da07cd5086
Renleilei <815994945@qq.com>
2017-04-20 00:22:41 +0800

    Update drag.js

--- js/drag.js
+++ js/drag.js
@@ -1,1 +1,29 @@
 
+;(function($){
+	$.fn.drag = function(){
+		var M = false;	//默认不移动
+		var Rx,Ry;
+		var t = $(this);
+		t.mousedown(function(e){
+			Rx = e.pageX-(parseInt(t.css("left")) || 0);
+			Ry = e.pageY-(parseInt(t.css("top")) || 0);
+			t.css({
+				"position":"fixed",
+				"cursor" :"move"
+			}).fadeTo(20,0.5);
+			M = true;
+		}).mouseup(function(e){
+			M = false; 
+			t.fadeTo(20,1);
+		});
+		$(document).mousemove(function(e){
+			if(M){
+				t.css({
+					top:e.pageY-Ry,
+					left:e.pageX-Rx
+				});
+			}
+		});
+	}
+
+})(jQuery);
